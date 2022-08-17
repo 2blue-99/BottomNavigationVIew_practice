655c05e5fdf0665b4034cf08e63cbae7aaa25e29
2blue <lpm0831@naver.com>
2022-08-17 23:17:04 +0900

    탭 두번 수정

--- app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.bottomnavigationview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation.findNavController
 import androidx.navigation.findNavController
@@ -16,20 +17,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         loadFragment(FirstFragment())
         bottomNav = findViewById(R.id.bottomNav) as BottomNavigationView
-        bottomNav.setOnNavigationItemReselectedListener {
+        bottomNav.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.home -> {
+                    Log.e(javaClass.simpleName, "onCreate: home", )
                     loadFragment(FirstFragment())
-                    return@setOnNavigationItemReselectedListener
+                    true
                 }
-                R.id.message -> {
+                R.id.recode -> {
+                    Log.e(javaClass.simpleName, "onCreate: recode", )
                     loadFragment(SecondeFragment())
-                    return@setOnNavigationItemReselectedListener
+                    true
+                }
+                else -> {
+                    false
                 }
             }
         }
     }
     private fun loadFragment(fragment: Fragment){
+        Log.e(javaClass.simpleName, "loadFragment : ", )
         val transaction = supportFragmentManager.beginTransaction()
         transaction.replace(R.id.container,fragment)
         transaction.addToBackStack(null)
